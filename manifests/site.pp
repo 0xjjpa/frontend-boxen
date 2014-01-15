47d9d9260cfe632f985b30d892e4837d4ffff93c
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2014-01-15 15:52:30 +0100

    Updated versions of NodeJS

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,9 +64,8 @@ node default {
   }
 
   # node versions
-  include nodejs::v0_6
-  include nodejs::v0_8
   include nodejs::v0_10
+  include nodejs::v0_11
 
   # default ruby versions
   include ruby::1_9_3
